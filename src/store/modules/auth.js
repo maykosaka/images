cb6297590a969f28a06340777060705a1529c753
May K <may.kosaka@gmail.com>
2018-09-15 03:48:26 +0900

    Add finalizeLogin action to parse and set the access token

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
     token: null
@@ -11,6 +12,11 @@ const getters = {
 const actions = {
     login: () => {
         api.login();
+    },
+    finalizeLogin({ commit }, hash) {
+        const queryString = qs.parse(hash.replace('#', ''));
+
+        commit('setToken', queryString.access_token);
     },
     logout: ({ commit }) => {
         commit('setToken', null);
