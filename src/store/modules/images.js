376b9e1026780232c56896f0286b8fe96aef608c
May K <may.kosaka@gmail.com>
2018-10-10 16:42:25 +0900

    Build images and access token into uploadImages

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -15,7 +15,13 @@ const actions = {
         commit('setImages', response.data.data);
     },
     async uploadImages({ commit }, images) {
-        console.log(images);
+        // Get the access token
+        const { token } = rootState.auth;
+
+        // Call our API module to do the upload
+        await api.upload(images, token);
+
+        // Redirect our user to ImageList component
     }
 };
 
